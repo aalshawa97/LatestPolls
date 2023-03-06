35a8a8969f4ebe8c10a2eb3b17e05d2eaf2309d3
abdullah97 <abdullahalshawa@gmail.com>
2023-03-06 07:26:19 -0800

    Removed unnecessary comments, and fixed formatting for create page!

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,4 +1,5 @@
 import os
+
 import django
 
 from pages.views import createPageView
@@ -8,17 +9,10 @@ django.setup()
 from django.contrib import admin
 from django.urls import path, include
 
-from pages import views as poll_views
-
 urlpatterns = [
-    #path("", views.index, name='index'),
     #Home
     path('', include('pages.urls')),
     #Django adminstration
     path('admin/', admin.site.urls),
     path('create/', createPageView, name='create'),
-    #path('vote', poll_views.votePageView('',0), name='vote'),
-    #path('', poll_views.homePageView, name='home')
-    #path('', pages.templates)
-    #path('about/', views.about, name='about'),
 ]
